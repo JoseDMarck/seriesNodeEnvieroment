745cf1821214bd8db9cd3d7d924277fc518eac5e
JoseDSaramago <josewbmx@gmail.com>
2023-05-02 04:17:57 -0600

    se creo el metodo para obtnener el numero primo

--- app/classes/Series.js
+++ app/classes/Series.js
@@ -1,10 +1,19 @@
 class Series {
 	constructor(number) {
 		this.number = number;
+		this.n_prime = null;
 	}
 
 	validateInputNumber() {}
-	getPrimeNumber() {}
+
+	getPrimeNumber() {
+		const number = this.n_prime;
+		if (number <= 1) return false;
+		for (let i = 2; i < number; i++) {
+			if (number % i === 0) return false;
+		}
+		return number;
+	}
 	getTriangularNumber() {}
 	getFibonacciNumber() {}
 }
