6cac76927ddd53b261e3778aa1867cb7b52670b4
JoseDSaramago <josewbmx@gmail.com>
2023-05-02 04:23:59 -0600

    Se creo el metodo para obtnener el numero fibonacci

--- app/classes/Series.js
+++ app/classes/Series.js
@@ -35,4 +35,17 @@ class Series {
 		}
 		return tNumber;
 	}
+
+	/* --------------------------------------------------------
+	/* ---- NUMERO FIBONACCI
+	-------------------------------------------------------- */
+
+	getFibonacciNumber() {
+		const number = this.n_fibonacci;
+		const fib = [0, 1];
+		for (let i = 2; i <= number; i++) {
+			fib[i] = fib[i - 2] + fib[i - 1];
+		}
+		return fib[number];
+	}
 }
