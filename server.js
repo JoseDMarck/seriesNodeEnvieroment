0d4334e741a1ab497ea51daa1309dd23862ee36b
JoseDSaramago <josewbmx@gmail.com>
2023-05-02 03:30:35 -0600

    instalacion de independiencias

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+
+var cors = require("cors");
+
+//const port = process.env.PORT || 3000;
+const hostname = "localhost";
+const port = 3000;
+
+// Parsing Json peticiones no mayores a 20mb
+app.use(
+	bodyParser.json({
+		limit: "40mb",
+	})
+);
+
+app.use(
+	bodyParser.urlencoded({
+		limit: "40mb",
+		extended: true,
+	})
+);
+
+//CORS
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(port, hostname, function () {
+	console.log(`Server running 🚀✨ at http://${hostname}:${port}/ `);
+});
