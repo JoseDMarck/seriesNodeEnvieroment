79d2bb41235148c69b030580c469ba66d49bd792
JoseDSaramago <josewbmx@gmail.com>
2023-05-02 04:30:12 -0600

    Se agrgeo el metodo para las validaciones en la clase Series

--- app/classes/Series.js
+++ app/classes/Series.js
@@ -3,10 +3,8 @@ class Series {
 		this.number = number;
 		this.n_prime = null;
 		this.n_triangular = null;
-		g;
+		this.n_fibonacci = null;
 	}
-
-	validateInputNumber() {}
 
 	/* --------------------------------------------------------
 	/* ---- NUMERO PRIMO
@@ -37,7 +35,7 @@ class Series {
 	}
 
 	/* --------------------------------------------------------
-	/* ---- NUMERO FIBONACCI
+	/* ---- NUGMERO FIBONACCI
 	-------------------------------------------------------- */
 
 	getFibonacciNumber() {
@@ -47,5 +45,32 @@ class Series {
 			fib[i] = fib[i - 2] + fib[i - 1];
 		}
 		return fib[number];
+	}
+	/* --------------------------------------------------------
+	/* ---- VALIDAR NUMERO 
+	-------------------------------------------------------- */
+	validateInputNumber() {
+		const number = this.number;
+
+		this.n_prime = this.number + 3; // (n+3)
+		this.n_triangular = this.number - 1; // (n+1)
+		this.n_fibonacci = this.number - 2; // (n-2)
+
+		//Valudar que el numero sea un Número
+		if (typeof number !== "number")
+			throw new Error("El número de entrada debe de ser un entero");
+
+		//Validar si es número Primo
+		if (!this.getPrimeNumber()) throw new Error("El número no es primo");
+
+		//Validar numero triangular
+		if (this.n_triangular < 1)
+			throw new Error("El número triangular debe de ser mayor a 0");
+
+		//Validar numero fibonacci
+		if (this.n_fibonacci < 1)
+			throw new Error("El número Fibonnaci debe de ser mayor a 0");
+
+		return true;
 	}
 }
